f79a2c2428f0700d4e179933fa9b5f1b0f592b93
SAGARA Tsukasa <sagara13579@gmail.com>
2010-07-30 02:13:09 +0900

    Comlete version.

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,52 @@
-# implement this
+use strict;
+use warnings;
+package Sorter;
+
+sub new {
+    my $class = shift;
+    my $self = { values=>[] };
+    return bless $self, $class;
+}
+
+sub set_values {
+    my $self = shift;
+    $self->{values} = [ @_ ];
+}
+
+sub get_values {
+    my $self = shift;
+    @{$self->{values}};
+}
+
+sub sort {
+    my $self = shift;
+    qsort($self->{values}, 0, $#{$self->{values}});
+}
+
+sub qsort {
+    my $array = shift;
+    my $left = shift;
+    my $right = shift;
+    my ($i, $j, $pivot, $tmp);
+    if ($left < $right) {
+        $i = $left;
+        $j = $right;
+        $pivot = $array->[($left+$right)/2];
+        while ($i <= $j) {
+            while ($array->[$i] < $pivot) { $i++; }
+            while ($array->[$j] > $pivot) { $j--; }
+            if ($i <= $j) {
+		$tmp = $array->[$i];
+		$array->[$i] = $array->[$j];
+		$array->[$j] = $tmp;
+		$i++;
+		$j--;
+            }
+        }
+        qsort($array, $left, $j);
+        qsort($array, $i, $right);
+    }
+}
+
+
+1;
